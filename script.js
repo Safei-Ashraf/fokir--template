e89d6f5c82ae3b249eea2af349a8781b0c2af3cc
ma9da <dodymasry25@gmail.com>
2022-03-08 12:18:51 +0200

    navbar color changed onn scroll

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ let navToggle = document.getElementsByClassName("navToggle")[0]
 let navLinks = document.getElementsByClassName("navLinks")[0]
 
 navToggle.addEventListener("click", menuDown);
-function menuDown(){
-    if(navLinks.style.display === "flex"){
-        navLinks.style.display="none"
-    }else{
-        navLinks.style.display="flex"
+function menuDown() {
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = "none"
+    } else {
+        navLinks.style.display = "flex"
+    }
+}
+
+let navbar = document.getElementsByClassName("navbar")[0]
+
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+        navbar.classList += " navColored";
+        navbar.classList.remove("navTransparent");
+    }
+    else {
+        navbar.classList += " navTransparent";
+        navbar.classList.remove("navColored");
     }
 }
