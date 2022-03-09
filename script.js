a5ada855cc17d4dd8eb467f2cc62b730c78c2890
ma9da <dodymasry25@gmail.com>
2022-03-09 11:35:00 +0200

    nav links disappearing solved

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let navToggle = document.getElementsByClassName("navToggle")[0]
 let navLinks = document.getElementsByClassName("navLinks")[0]
 
 navToggle.addEventListener("click", menuDown);
-function menuDown(){
-    if(navLinks.style.display === "flex"){
-        navLinks.style.display="none"
-    }else{
-        navLinks.style.display="flex"
+function menuDown() {
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = "none"
+    } else {
+        navLinks.style.display = "flex"
     }
 }
