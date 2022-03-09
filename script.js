513fa600e29279be58ec6d4d0941ac056e22ad3b
ma9da <dodymasry25@gmail.com>
2022-03-09 11:59:47 +0200

    remove unsed code

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let TxtRotate = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
+    this.period = parseInt(period, 10) || 500;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
